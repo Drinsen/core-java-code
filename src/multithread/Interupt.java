bc720f77b6c5de5ad65048ac87449f15b04dbf60
Liujunxin <junxin.liu@ucarinc.com>
2020-04-20 14:34:09 +0800

    1

--- src/multithread/Interupt.java
+++ src/multithread/Interupt.java
@@ -11,35 +11,24 @@ public class Interupt {
     public static void main(String[] args) throws Exception {
         Thread t = new Thread(new Worker());
         t.start();
-
-        Thread.sleep(100);
-        t.interrupt();
-
+        Thread.sleep(1000);
+//        t.interrupt();
         System.out.println("Main thread stopped.");
     }
 
     public static class Worker implements Runnable {
         @Override
         public void run() {
+            Thread thread = Thread.currentThread();
+            System.out.println("打断之前的标志位  "+thread.isInterrupted());
+            thread.interrupt();
+            System.out.println("打断之后的标志位  "+thread.isInterrupted());
+            System.out.println("interrupted 结果 "+Thread.interrupted());;
+            System.out.println("再次打断之后的标志位  "+thread.isInterrupted());
+            System.out.println("11111");
+            System.out.println(22222);
             System.out.println("Worker started.");
-
-            try {
-                Thread.sleep(500); // 此时被interrupt()会抛出InterruptedException
-            } catch (InterruptedException e) {
-                Thread thread = Thread.currentThread();
-                System.out.println("再次中断之前isInterrupted():" + thread.isInterrupted());
-                System.out.println("再次中断之前interrupted():" + Thread.interrupted());
-                // 再次调用interrupt方法中断自己，将中断状态设置为“中断”
-                thread.interrupt();
-                System.out.println("再次interrupt()后isInterrupted():" + thread.isInterrupted());
-                System.out.println("再次interrupt()后第一次interrupted()返回:" + Thread.interrupted());// clear status
-                // interrupted()判断是否中断，还会清除中断标志位
-                System.out.println("interrupted()后此时再判断IsInterrupted: " + thread.isInterrupted());
-                System.out.println("---------After Interrupt Status Cleared----------");
-                System.out.println("再次interrupt()后第二次interrupted()返回: " + Thread.interrupted());
-                System.out.println("此时再判断IsInterrupted: " + thread.isInterrupted());
             }
-            System.out.println("Worker stopped.");
         }
     }
-}
+
